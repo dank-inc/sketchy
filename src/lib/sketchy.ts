dcdfecdd46155ae65c47a427fc3af067638235cb
Elijah Lucian <elijahlucian@gmail.com>
2021-04-04 11:05:10 -0600

    removing console logging

--- src/lib/sketchy.ts
+++ src/lib/sketchy.ts
@@ -2,13 +2,11 @@ import { Frame, Sketch, SketchParams } from './types'
 
 const animateSketch = (frame: Frame, params: SketchParams) => {
   frame(params)
-  console.log('animated!')
-
   // TODO: delta time
 
-  requestAnimationFrame(() =>
-    animateSketch(frame, { ...params, time: (params.time += 0.01) }),
-  )
+  const time = params.time + 0.01
+
+  requestAnimationFrame(() => animateSketch(frame, { ...params, time }))
 }
 
 export const loadSketch = (sketch: Sketch, params: SketchParams) => {
