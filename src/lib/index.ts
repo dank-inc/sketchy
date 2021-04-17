a75f11663e528d6938f74800fde4a219d9aedca7
Elijah Lucian <elijahlucian@gmail.com>
2021-04-17 15:06:51 -0600

    ✌

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,6 @@
 export { createParams } from './config'
 export { createSketch, loadSketch } from './sketchy'
-export { Sketch, Frame, SketchParams, SketchConfig } from './types'
+export { Sketch, Frame, SketchyParams, SketchConfig } from './types'
 
 export * from './types/common'
 
